aacddeddf2fe2d1df83373a7f2e89c2ac099ffc2
GraphTree <graphnodesed@gmail.com>
2022-07-05 21:23:58 +0900

    added test coment

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -3,5 +3,6 @@ import {collatedTasks} from '../constants';
 
 export const getCollatedTasksExist = selecteProject => {
     // collatedTasksExist will be added later
+    // test coment
     collatedTasksExist.find(task => task.key === selectedProject); 
 }
