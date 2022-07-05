48b63a0237c3bd603a44da932d8560dfe68485cf
GraphTree <graphnodesed@gmail.com>
2022-07-05 21:28:20 +0900

    added test coment on css

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,5 +2,6 @@
 import {collatedTasks} from '../constants';
 
 export const getCollatedTasksExist = selecteProject => {
+    // assume we added css file
     collatedTasksExist.find(task => task.key === selectedProject); 
 }
