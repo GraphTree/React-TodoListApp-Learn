cb53004ba011ade8cd885fe00955498b05abbcf7
GraphTree <graphnodesed@gmail.com>
2022-07-05 20:57:27 +0900

    Revert "test commit point"
    This reverts commit 5e2058adda3373020033078a109b58a504858e98.

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,6 +2,5 @@
 import {collatedTasks} from '../constants';
 
 export const getCollatedTasksExist = selecteProject => {
-    // collatedTasksExist will be added later
     collatedTasksExist.find(task => task.key === selectedProject); 
 }
