5e2058adda3373020033078a109b58a504858e98
GraphTree <graphnodesed@gmail.com>
2022-07-05 16:57:01 +0900

    test commit point

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,5 +2,6 @@
 import {collatedTasks} from '../constants';
 
 export const getCollatedTasksExist = selecteProject => {
+    // collatedTasksExist will be added later
     collatedTasksExist.find(task => task.key === selectedProject); 
 }
