db5d19858f5c007793e17ec2e3e51ab0d7807fef
GraphTree <graphnodesed@gmail.com>
2022-07-08 11:35:34 +0900

    created collatedTasksExist helper function

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,9 +1,6 @@
 
 import {collatedTasks} from '../constants';
 
-export const getCollatedTasksExist = selecteProject => {
-
-    // assume we added css file
-    //test for rebase amend
-    collatedTasksExist.find(task => task.key === selectedProject); 
+export const collatedTasksExist = selectedProject => {
+    collatedTasks.find(task => task.key === selectedProject); 
 }
